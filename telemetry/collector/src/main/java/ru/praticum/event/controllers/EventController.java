14f7f9962b68fd67d2447d538676fbeff8ff140a
Matvey <matvey.oleynik.2017@mail.ru>
2025-07-28 16:56:26 +0300

    beginning(1)

---
+++ telemetry/collector/src/main/java/ru/praticum/event/controllers/EventController.java
@@ -0,0 +1,10 @@
+package ru.praticum.event.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/events")
+public class EventController {
+    @PostMapping("/sensor")
+    public void
+}
---
+++ telemetry/collector/src/main/java/ru/praticum/event/controllers/Main.java
@@ -0,0 +1,7 @@
+package ru.praticum.event.controllers;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@
+public class Main {
+}
