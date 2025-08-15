a08c204adcb470f5a8b37499616207c25eefcdf2
mat973 <matvey.oleynik.2017@mail.ru>
2025-08-15 22:51:43 +0300

    i hate mapstruct,fix naming problem idk(6)

--- telemetry/collector/src/main/java/ru/practicum/event/mapper/hub/mapstruct/HubProtoToJavaMapper.java
+++ telemetry/collector/src/main/java/ru/practicum/event/mapper/hub/mapstruct/HubProtoToJavaMapper.java
@@ -38,24 +38,55 @@ public interface HubProtoToJavaMapper {
     @Mapping(target = "name", source = "scenarioRemoved.name")
     ScenarioRemovedEvent scenarioRemovedToJava(HubEventProto proto);
 
-    Conditions toJava(ScenarioConditionProto proto);
-    Actions toJava(DeviceActionProto proto);
+
+    default Conditions toJava(ScenarioConditionProto proto) {
+        if (proto == null) return null;
+        Conditions c = new Conditions();
+        c.setSensorId(proto.getSensorId());
+        c.setType(map(proto.getType()));
+        c.setOperation(map(proto.getOperation()));
+        if (proto.hasBoolValue()) {
+            c.setValue(proto.getBoolValue() ? 1 : 0);
+        } else if (proto.hasIntValue()) {
+            c.setValue(proto.getIntValue());
+        } else {
+            c.setValue(null);
+        }
+        return c;
+    }
+
+    default Actions toJava(DeviceActionProto proto) {
+        if (proto == null) return null;
+        Actions a = new Actions();
+        a.setSensorId(proto.getSensorId());
+        a.setType(map(proto.getType()));
+        a.setValue(proto.hasValue() ? proto.getValue() : null);
+        return a;
+    }
+
 
     default DeviceType map(DeviceTypeProto proto) {
         return proto == null || proto == DeviceTypeProto.UNRECOGNIZED
                 ? null
                 : DeviceType.valueOf(proto.name());
     }
+
     default ConditionType map(ConditionTypeProto proto) {
-        return proto == null || proto == ConditionTypeProto.UNRECOGNIZED ? null : ConditionType.valueOf(proto.name());
+        return proto == null || proto == ConditionTypeProto.UNRECOGNIZED
+                ? null
+                : ConditionType.valueOf(proto.name());
     }
 
     default OperationType map(ConditionOperationProto proto) {
-        return proto == null || proto == ConditionOperationProto.UNRECOGNIZED ? null : OperationType.valueOf(proto.name());
+        return proto == null || proto == ConditionOperationProto.UNRECOGNIZED
+                ? null
+                : OperationType.valueOf(proto.name());
     }
 
     default ActionsType map(ActionTypeProto proto) {
-        return proto == null || proto == ActionTypeProto.UNRECOGNIZED ? null : ActionsType.valueOf(proto.name());
+        return proto == null || proto == ActionTypeProto.UNRECOGNIZED
+                ? null
+                : ActionsType.valueOf(proto.name());
     }
 
     default Instant toInstant(com.google.protobuf.Timestamp ts) {
