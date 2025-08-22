54e93afa7a718d79436f0135c501c4935d34fb4b
mat973 <matvey.oleynik.2017@mail.ru>
2025-08-22 20:45:34 +0300

    IHM hot fix add chane property 1

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/config/KafkaConfig.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/config/KafkaConfig.java
@@ -6,7 +6,6 @@ import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
