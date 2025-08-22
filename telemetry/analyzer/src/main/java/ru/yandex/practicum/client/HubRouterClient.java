fdee911435c4b1b17e41de4e45d7bcbb1f97032a
mat973 <matvey.oleynik.2017@mail.ru>
2025-08-22 19:54:07 +0300

    IHM hot fix some change )(15)

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/client/HubRouterClient.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/client/HubRouterClient.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.client;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.grpc.telemetry.event.DeviceActionRequest;
@@ -9,6 +10,7 @@ import ru.yandex.practicum.grpc.telemetry.hubrouter.HubRouterControllerGrpc;
 import com.google.protobuf.Empty;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class HubRouterClient {
 
@@ -17,7 +19,12 @@ public class HubRouterClient {
     private HubRouterControllerGrpc.HubRouterControllerBlockingStub hubRouterStub;
 
     public void sendAction(DeviceActionRequest request) {
-        hubRouterStub.handleDeviceAction(request);
+        try {
+            hubRouterStub.handleDeviceAction(request);
+            log.info("Команда отправлена на Hub Router: {}", request);
+        } catch (Exception e) {
+            log.error("Не удалось отправить команду на Hub Router", e);
+        }
     }
 }
 
